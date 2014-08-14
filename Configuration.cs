0ed8213a32dec97b821add1a687db650549e89b6
James Dingle <ding_jimmy@hotmail.com>
2014-08-14 16:47:48 +0100

    Update and rename ActiveRecord Globals.cs to Configuration.cs

---
+++ Configuration.cs
@@ -0,0 +1,17 @@
+namespace Level
+{
+
+  public class Database
+  {
+    
+    public static string Name { get; set; }
+    
+    public static string Server { get; set; }
+    
+    public static string ProviderName { get; set; }
+    
+    public static DbProviderFactory Provider { get; set; }
+    
+  }
+  
+}
